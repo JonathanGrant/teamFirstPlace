c1b97bcd2b6aa9170d06eef9b49f9aba4ddec3a0
Jonathan Grant <jonathanallengrant@gmail.com>
2016-03-03 21:00:59 -0800

    done

--- code/teamFirstPlace.py
+++ code/teamFirstPlace.py
@@ -13,7 +13,7 @@ class Run:
         self.servo = servo
         self.odometry = odometry.Odometry()
         # self.pidTheta = pd_controller2.PDController(500, 100, -200, 200, is_angle=True)
-        self.pidTheta = pid_controller.PIDController(300, 5, 50, [-10, 10], [-200, 200], is_angle=True)
+        self.pidTheta = pid_controller.PIDController(200, 5, 50, [-10, 10], [-200, 200], is_angle=True)
         self.pidDistance = pid_controller.PIDController(1000, 0, 50, [0, 0], [-200, 200], is_angle=False)
         self.waypoints = [[2 ,0], [2 ,1], [0 ,1], [0 ,0]]
 
@@ -39,10 +39,25 @@ class Run:
                     goal_theta = math.atan2(goal_y - self.odometry.y, goal_x - self.odometry.x)
                     theta = math.atan2(math.sin(self.odometry.theta), math.cos(self.odometry.theta))
                     output_theta = self.pidTheta.update(self.odometry.theta, goal_theta, self.time.time())
-					
+                    self.distanceFromObstacle = self.sonar.get_distance()
+                    print(self.distanceFromObstacle)
                     # improved version 2: fuse with velocity controller
                     distance = math.sqrt(math.pow(goal_x - self.odometry.x, 2) + math.pow(goal_y - self.odometry.y, 2))
                     output_distance = self.pidDistance.update(0, distance, self.time.time())
-                    self.create.drive_direct(int(output_theta + output_distance)+base_speed, int(-output_theta + output_distance)+base_speed)
-                    if distance < 0.1:
-                        break
+                    if self.distanceFromObstacle <= 1.0:
+#                        if output_theta < -60:
+#                            output_theta = -60
+#                        elif output_theta > 60:
+#                            output_theta = 60
+                            
+                        self.servo.go_to(output_theta)
+                        self.create.drive_direct(int(output_theta + output_distance)+base_speed, -int(output_theta + output_distance)+base_speed)
+                        print(self.distanceFromObstacle)
+                    else:
+                        self.create.drive_direct(int(output_theta + output_distance)+base_speed, int(-output_theta + output_distance)+base_speed)
+                        self.servo.go_to(-output_theta)
+                        if distance < 0.1:
+                            break
+
+
+                        
