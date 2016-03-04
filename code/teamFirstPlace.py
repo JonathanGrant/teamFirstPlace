ae4fd1e2e261c52d561ff10b10901981aa08df65
Jonathan Grant <jonathanallengrant@gmail.com>
2016-03-03 18:29:27 -0800

    really messing up

--- code/teamFirstPlace.py
+++ code/teamFirstPlace.py
@@ -15,7 +15,7 @@ class Run:
         # self.pidTheta = pd_controller2.PDController(500, 100, -200, 200, is_angle=True)
         self.pidTheta = pid_controller.PIDController(300, 5, 50, [-10, 10], [-200, 200], is_angle=True)
         self.pidDistance = pid_controller.PIDController(1000, 0, 50, [0, 0], [-200, 200], is_angle=False)
-		self.waypoints = [[2 ,0], [2 ,1], [0 ,1], [0 ,0]]
+        self.waypoints = [[2 ,0], [2 ,1], [0 ,1], [0 ,0]]
 
     def run(self):
         self.create.start()
@@ -28,24 +28,21 @@ class Run:
         ])
 
         for goal_x, goal_y in self.waypoints:
-			base_speed = 100
-			start_time = self.time.time()
-
-			with open('output.csv', 'w') as f:
-				while True:
-					state = self.create.update()
-					if state is not None:
-						self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
-						goal_theta = math.atan2(goal_y - self.odometry.y, goal_x - self.odometry.x)
-						theta = math.atan2(math.sin(self.odometry.theta), math.cos(self.odometry.theta))
-						f.write("{},{},{}\n".format(self.time.time() - start_time, theta, goal_theta))
-						print("[{},{},{}]".format(self.odometry.x, self.odometry.y, math.degrees(self.odometry.theta)))
-
-						output_theta = self.pidTheta.update(self.odometry.theta, goal_theta, self.time.time())
-
-						# improved version 2: fuse with velocity controller
-						distance = math.sqrt(math.pow(goal_x - self.odometry.x, 2) + math.pow(goal_y - self.odometry.y, 2))
-						output_distance = self.pidDistance.update(0, distance, self.time.time())
-						self.create.drive_direct(int(output_theta + output_distance), int(-output_theta + output_distance))
-						if distance < 0.1:
-                    		break
+            base_speed = 100
+            start_time = self.time.time()
+            print("0")
+            while True:
+                print("1")
+                state = self.create.update()
+                if state is not None:
+                    self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
+                    goal_theta = math.atan2(goal_y - self.odometry.y, goal_x - self.odometry.x)
+                    theta = math.atan2(math.sin(self.odometry.theta), math.cos(self.odometry.theta))
+                    output_theta = self.pidTheta.update(self.odometry.theta, goal_theta, self.time.time())
+					
+                    # improved version 2: fuse with velocity controller
+                    distance = math.sqrt(math.pow(goal_x - self.odometry.x, 2) + math.pow(goal_y - self.odometry.y, 2))
+                    output_distance = self.pidDistance.update(0, distance, self.time.time())
+                    self.create.drive_direct(int(output_theta + output_distance), int(-output_theta + output_distance))
+                    if distance < 0.1:
+                        break
