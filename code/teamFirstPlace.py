b40832c686b5b25a1d98cbc636b2fd4bfc110135
Jonathan Grant <jonathanallengrant@gmail.com>
2016-03-03 19:16:11 -0800

    plz CHange

--- code/teamFirstPlace.py
+++ code/teamFirstPlace.py
@@ -14,7 +14,8 @@ class Run:
         self.odometry = odometry.Odometry()
         # self.pidTheta = pd_controller2.PDController(500, 100, -200, 200, is_angle=True)
         self.pidTheta = pid_controller.PIDController(300, 5, 50, [-10, 10], [-200, 200], is_angle=True)
-        self.pidDistance = pid_controller.PIDController(1000, 0, 50, [0, 0], [-200, 200], is_angle=False)
+        self.pidDistance = pid_controller.PIDController(500, 0, 50, [0, 0], [-200, 200], is_angle=False)
+        self.pidObstacle = pid_controller.PIDController(500,10,75,[-1,1], [-200, 200], is_angle=False)
         self.waypoints = [[2 ,0], [2 ,1], [0 ,1], [0 ,0]]
 
     def run(self):
@@ -30,6 +31,7 @@ class Run:
         print("Ready, Set, GO!")
         atObstacle = False
         distanceFromObstacle = 3.3
+        scanAngle = 0
 
         for goal_x, goal_y in self.waypoints:
             base_speed = 100
@@ -40,7 +42,13 @@ class Run:
                     if not atObstacle:
                         base_speed = 100
                         self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
-                        goal_theta = math.atan2(goal_y - self.odometry.y, goal_x - self.odometry.x)
+						
+						#Scan environment for obstacle
+                        distanceFromObstacle = self.sonar.get_distance()
+                        if distanceFromObstacle <= 1.0:
+                        	goal_theta = math.atan2(45 - self.odometry.y, 45 - self.odometry.x)
+                        else:
+							goal_theta = math.atan2(goal_y - self.odometry.y, goal_x - self.odometry.x)
                         theta = math.atan2(math.sin(self.odometry.theta), math.cos(self.odometry.theta))
                         output_theta = self.pidTheta.update(self.odometry.theta, goal_theta, self.time.time())
 
@@ -50,14 +58,3 @@ class Run:
                         self.create.drive_direct(int(output_theta + output_distance)+base_speed, int(-output_theta + output_distance)+base_speed)
                         if distance < 0.1:
                             break
-                        #Scan environment for obstacle
-                        # distanceFromObstacle = self.sonar.get_distance()
-                        if distanceFromObstacle <= 1.5:
-                            atObstacle = True
-                    else:
-                        base_speed = 0
-                        #Scan environment for obstacle
-                        distanceFromObstacle = self.sonar.get_distance()
-                        if distanceFromObstacle >= 1.5:
-                            atObstacle = False
-                        print(distanceFromObstacle)
